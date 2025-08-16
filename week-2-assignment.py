3f110f37c72e7f12d2461924b49ab39de50fdb52
karisgit <karisbrainiac@gmail.com>
2025-08-16 10:43:30 +0300

    Completed and submitted week-2 assignment

---
+++ week-2-assignment.py
@@ -0,0 +1,27 @@
+# Question-1  Creating an empty list called my_list
+my_list = []
+
+# 2.  Append the elements 10, 20, 30, 40 to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Inserting the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# 4. Extending my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+
+# 5. Remove the last element from my_list
+my_list.pop()
+
+# 6. Sort my_list in ascending order
+my_list.sort()
+
+# 7.  Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+
+print("The index of 30 is:", index_of_30)
+
